10f536630441f3863c82d6bebd4b0ea78ece5b8e
Yohann Bethoule <yohann.bethoule@gmail.com>
2023-12-03 19:31:24 +0100

    Updated main.go

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "AdventOfCode2023/day1"
+import (
+	"AdventOfCode2023/day3"
+)
 
 func main() {
-	day1.Star1()
-	day1.Star2()
+	//day1.Star1()
+	//day1.Star2()
+
+	//day2.Star1()
+	//day2.Star2()
+
+	day3.Star1()
+	day3.Star2()
 }
